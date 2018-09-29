0be76d4c2132046729e9d9dd9311a26eb202b235
ariiiman <aristide.benoist@gmail.com>
2018-09-29 13:07:53 +0200

    2.1.7

--- src/Scroll/TopWhenRefresh.js
+++ src/Scroll/TopWhenRefresh.js
@@ -11,7 +11,11 @@ S.TopWhenRefresh()
 */
 
 S.TopWhenRefresh = function () {
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0)
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual'
+    } else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0)
+        }
     }
 }
